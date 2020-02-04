e8da02e9a7cd5aa1be4e6d437b9dc0fb220b0465
TigoDEV <me@tigodev.com>
2020-02-04 11:27:04 +0100

    Init Resource Project

---
+++ __resource.lua
@@ -0,0 +1,16 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+name 'ESX_CustomJobs'
+author 'TigoDevelopment'
+contact 'me@tigodev.com'
+version '0.0.1'
+
+description 'This is a repsitory where you can create your own jobs and it is easier to expand it. Here jobs have a possibility for an F6 menu.'
+
+server_scripts {
+    'server/main.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
